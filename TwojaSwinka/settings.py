e24b3e88991a3f2b6a3db4f263d7f73ebf794b9f
maciejwitos <mac.witos@gmail.com>
2020-04-10 14:05:37 +0200

    add database details

--- TwojaSwinka/settings.py
+++ TwojaSwinka/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'TwojaSwinka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'twojaswinkadatabase',
+        'USER': 'maciejwitos',
+        'PASSWORD': 'PostgreSQL2020',
+        'HOST': 'twojaswinkadatabase.cv9ikkunke92.eu-central-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
