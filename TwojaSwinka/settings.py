af1f9fd2aa025b62f7bae08fea9acd1582be05b4
Ubuntu <ubuntu@ip-172-31-36-235.eu-central-1.compute.internal>
2020-04-14 15:19:47 +0000

    fixed bug with import

--- TwojaSwinka/settings.py
+++ TwojaSwinka/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from TwojaSwinka.local_config import *
+#from TwojaSwinka.local_config import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +85,14 @@ DATABASES = {
 }
 
 WSGI_APPLICATION = 'TwojaSwinka.wsgi.application'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'twoja.swinka.pl@gmail.com'
+EMAIL_HOST_PASSWORD = 'Twoja$winka2020'
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = 'Twoja Świnka twoja.swinka.pl@gmail.com'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
